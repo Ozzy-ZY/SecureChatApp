c3e2f9c1e0bf06924f2ca0878095c592e92e748c
seif <92771610+Seif357@users.noreply.github.com>
2023-12-14 07:32:08 +0200

    Update Source.cpp

--- SecureChatApp/Source.cpp
+++ SecureChatApp/Source.cpp
@@ -1,24 +1,22 @@
 #include <iostream>
 #include <winsock.h>
 using namespace std;
-#define Port 9909
-
-struct sockaddr_in srv;
+#define Port 9909//The port number is a communication endpoint in a computer network, and it is used to uniquely identify a specific process to which data or messages should be sent. In the context of networking, a port is associated with an IP address to establish a connection between two devices.
+struct sockaddr_in srv;//, the server address is represented by the struct sockaddr_in srv structure. This structure is used to configure the server's network address, including details like the address family, port number, and IP address.
 fd_set fr, fe, fw;
-int nmaxfd;
+int nMaxFd;
 int nSocket;
 int nArrClient[5];//for real life application u need more than 5
-//latest
 void ProcessNewMessages(int nClientSocket)
 {
-	cout << endl << "Processing the new message for client socket:" << nClientSocket;
-	char buff[256 + 1] = { 0, };
-	int nRet = recv(nClientSocket, buff, 256, 0);
-	if (nRet < 0)
+	cout << endl << "Processing the new message for client socket:" << nClientSocket; // Display a message indicating the start of message processing for a specific client socket
+	char buff[256 + 1] = { 0, }; // Initialize a buffer to store the received message
+	int nRet = recv(nClientSocket, buff, 256, 0);// Receive the message from the client socket
+	if (nRet < 0) // Check if an error occurred during message reception
 	{
-		cout << endl << "Something wrong happeng..closing the connection for client";
+		cout << endl << "Something wrong happeng..closing the connection for client";// Display an error message and close the connection for the client socket
 		closesocket(nClientSocket);
-		for (int nIndex = 0; nIndex < 5; nIndex++)
+		for (int nIndex = 0; nIndex < 5; nIndex++) // Remove the client socket from the array of client sockets (assuming nArrClient is a global array)
 		{
 			if (nArrClient[nIndex] == nClientSocket)
 			{
@@ -29,10 +27,10 @@ void ProcessNewMessages(int nClientSocket)
 	}
 	else
 	{
-		cout << endl << "the message recevied from client is:" << buff;
+		cout << endl << "the message recevied from client is:" << buff; // Display the received message from the client
 		//send the response to client
 		send(nClientSocket, "processed your request", 23, 0);
-		cout << endl << "**********************************************";
+		cout << endl << "**********************************************"; // Display a separator for better visualization
 	}
 }
 void ProccessTheNewRequest()
@@ -43,8 +41,8 @@ void ProccessTheNewRequest()
 		int nClientSocket = accept(nSocket, NULL, &nLen);
 		if (nClientSocket > 0)
 		{
-			int nIndex;
 			//Put it into client fd_set
+			int nIndex;
 			for (nIndex = 0; nIndex < 5; nIndex++)
 			{
 				if (nArrClient[nIndex] == 0)
@@ -65,7 +63,7 @@ void ProccessTheNewRequest()
 	{
 		for (int nIndex = 0; nIndex < 5; nIndex++)
 		{
-			if (FD_ISSET(nArrClient[nIndex], & fr))
+			if (FD_ISSET(nArrClient[nIndex], &fr))
 			{
 				/*got the new message from the client
 				just rec new message
@@ -75,70 +73,53 @@ void ProccessTheNewRequest()
 		}
 	}
 }
-
 int main() {
 	int nRet = 0;
 	//uses wsa version 2.2
 	WSADATA ws;
-	if (WSAStartup(MAKEWORD(2, 2), &ws) != 0) {
+	if (WSAStartup(MAKEWORD(2, 2), &ws) != 0){
 		cout << "WSAStartup failed" << endl;
 		WSACleanup();
-		exit(EXIT_FAILURE);
-	}
+		exit(EXIT_FAILURE);}
 	else {
 		cout << "WSAStartup succeeded" << endl;
 	}
 	//makes a socket that uses tcp
 	nSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (nSocket == INVALID_SOCKET) {
+	if (nSocket == INVALID_SOCKET){
 		cout << "Socket creation failed" << endl;
 		WSACleanup();
 		exit(EXIT_FAILURE);
 	}
-	else {
+	else{
 		cout << "Socket created: " << nSocket << endl;
 	}
-	/* structure is initialized to set up the server's address information and specifies
-	the address family, port (converted to network byte order using htons)allows connections
-	from any available network interface */
-	srv.sin_family = AF_INET;//AF_INET indicates that this server will be using IPv4 addresses.
-	srv.sin_port = htons(Port);//You've used htons(port) to convert the port value to network byte order
+	/* structure is initialized to set up the server's address information and specifies the address family, port (converted to network byte order using htons)allows connections from any available network interface */
+	srv.sin_family = AF_INET;//sin_family: This field specifies the address family, and AF_INET indicates that the server will be using IPv4 addresses.
+	srv.sin_port = htons(Port);//This means that the server will be reachable on port 9909 for incoming client connections.//You've used htons(port) to convert the port value to network byte order using htons (host to network short).
 	srv.sin_addr.s_addr = INADDR_ANY;//This field specifies the IP address to which the server should bind. By setting it to INADDR_ANY, you are instructing the server to listen on all available network interfaces and IP addresses
-	//if you want to enter a specified ip adress srv.sin_addr.s_addr = inet_addr ("127.0.0.01");
-
+	//if you want to enter a specified ip adress srv.sin_addr.s_addr = inet_addr ("127.0.0.01"); for the loopback address. //we collect This information bec, it is used when binding the server socket to a specific address and when accepting incoming client connections.
+	memset(& (srv.sin_zero), 0, 8);
 	//setsocketopt
-	int noptval = 0;
-	int noptlen = sizeof(noptval);
-	nRet = setsockopt(nSocket, SOL_SOCKET, SO_REUSEADDR, (const char*)&noptval, noptlen);//if you want to change the adress every time then you chould use SO_EXCLUSIVEADDRUSE insted of SO_REUSEADDR
-	if(!nRet)
+
+	int nOptVal = 0;
+	int nOptLen = sizeof(nOptVal);
+	nRet = setsockopt(nSocket, SOL_SOCKET, SO_REUSEADDR, (const char*)&nOptVal, nOptLen);//if you want to change the adress every time then you chould use SO_EXCLUSIVEADDRUSE insted of SO_REUSEADDR
+	if (!nRet)
 	{
 		cout << endl << "the set socket call success";
 	}
 	else
-	{
-		cout << endl << "the set socket call failed";
+	{cout << endl << "the set socket call failed";
 		closesocket(nSocket);
 		WSACleanup();
 		exit(EXIT_FAILURE);
 	}
-
-	/*//blocking  vs non blocking sockets
-	// optval =0 blocking optval != 0 non blocking
-	u_long optval = 0;
-	nRet = ioctlsocket(nsocket, FIONBIO, &optval)
-		if (nRet != 0)
-		{
-			cout << endl << "ioclsocket call failed";
-		}
-		else
-		{
-			cout << endl << "ioclsocket call success";
-		}*/
-
+/*//blocking  vs non blocking sockets// optval =0 blocking optval != 0 non blockingu_long optval = 0;nRet = ioctlsocket(nsocket, FIONBIO, &optval)if (nRet != 0){cout << endl << "ioclsocket call failed";}else{			cout << endl << "ioclsocket call success";}*/
 	//associates the socket with the server's address and port.
 	nRet = bind(nSocket, (sockaddr*)&srv, sizeof(sockaddr));
-	if (nRet == SOCKET_ERROR) {
-		cerr << "Bind failed" << endl;
+	if (nRet < 0) 
+	{ cout << "Bind failed" << endl;
 		closesocket(nSocket);
 		WSACleanup();
 		exit(EXIT_FAILURE);
@@ -148,56 +129,51 @@ int main() {
 	}
 	// starts listening on the socket for incoming client connections on the port the developer created
 	nRet = listen(nSocket, 5);//5, represents the maximum number of pending connections.
-	if (nRet == SOCKET_ERROR) {
-		cerr << "Listen failed" << endl;
+	if (nRet < 0) 
+	{   cout << "Listen failed" << endl;
 		closesocket(nSocket);
 		WSACleanup();
 		return EXIT_FAILURE;
 	}
-	else {
+	else
+	{
 		cout << "Listening on port " << Port << endl;
 	}
 	//keep listening
-	int nmaxfd = nSocket;
+	nMaxFd = nSocket;
 	struct timeval tv;
-	tv.tv_sec = 0;
+	tv.tv_sec = 1;
 	tv.tv_usec = 0;
 	while (1) {
 		FD_ZERO(&fr);
 		FD_ZERO(&fe);
 		FD_ZERO(&fw);
+
 		FD_SET(nSocket, &fr);
 		FD_SET(nSocket, &fe);
-		cout << endl << "before select call:" << fr.fd_count;
-		nRet = select(nmaxfd + 1, &fr, &fw ,&fe, &tv);//The select function is used to wait for activity on the socket. 
+
+		for (int nIndex = 0; nIndex < 5; nIndex++)
+		{
+			if (nArrClient[nIndex] != 0)
+			{
+				FD_SET(nArrClient[nIndex], &fr);
+				FD_SET(nArrClient[nIndex], &fe);
+				
+			}
+		}
+
+		nRet = select(nMaxFd + 1, &fr, &fw, &fe, &tv);//The select function is used to wait for activity on the socket. 
 		if (nRet > 0)
 		{
-			//when someone connects or communicates with a message over a dedicated  connection
+			//when someone connects or communicates with a message over a dedicated  connection/*please remember that the socket listening to new clients and then communicating with the same client later are not the same.*//*after connection, you get one more socket to communicate with client you need to send/receive messages over the network using that new socket.*/
 			cout << "someone connected";
 			cout << "data on port ......processing now....";
-			//process the request
-			/*if (FD_ISSET(nsocket, &fe))
-			{
-				cout << endl << "there is an exception.just get away from here";
-			}
-			if (FD_ISSET(nsocket, &fw))
-			{
-				cout << endl << "ready to write sth";
-			}
-			if (FD_ISSET(nsocket, &fr))
-			{
-				cout << endl << "ready to read. sth new came up at the port";
-
-			}
-			break;
-			// Add code here for accepting incoming connections and handling them.
-		*/
+			//process the request/*if (FD_ISSET(nsocket, &fe)){cout << endl << "there is an exception.just get away from here";}if (FD_ISSET(nsocket, &fw)){cout << endl << "ready to write sth";}if (FD_ISSET(nsocket, &fr)){cout << endl << "ready to read. sth new came up at the port";//acept the new connection}break;// Add code here for accepting incoming connections and handling them.
 			ProccessTheNewRequest();
 		}
 		else if (nRet == 0)
 		{
 			// No incoming connections or data ready
-			cout << endl << "nothing on port:" << Port << fr.fd_count;
 		}
 		else
 		{
@@ -207,10 +183,6 @@ int main() {
 			WSACleanup();
 			exit(EXIT_FAILURE);
 		}
-		cout << endl << "after select call:" << fr.fd_count;
-
-
-		
 	}
 	// Clean up the socket and Winsock when done
 	closesocket(nSocket);
